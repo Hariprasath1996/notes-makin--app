22b4b1b0f5a10d74bb0e7eb3438663e12169f4a5
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-23 18:04:04 +0530

    js logics created a few

---
+++ script.js
@@ -0,0 +1,10 @@
+let container=document.getElementById("container")
+let popupOverlay = document.getElementById("popup-overlay")
+let popupBox=document.getElementById("popup-box")
+let popupBtn =document.getElementById("popup-btn")
+
+function addPopup(){
+    
+    popupOverlay.style.display="block"
+    popupBox.style.display="block"
+}

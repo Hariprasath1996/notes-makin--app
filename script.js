5a138fcedc06d236f006f90fab9e896435913dbf
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-24 11:08:35 +0530

    event listener created

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let container=document.getElementById("container")
+// select popupOverlay, popupBox , popupBtn 
 let popupOverlay = document.getElementById("popup-overlay")
 let popupBox=document.getElementById("popup-box")
 let popupBtn =document.getElementById("popup-btn")
+let btnAdd=document.getElementById("btn-add")
+let btnRemove=document.getElementById("btn-remove")
 
-function addPopup(){
-    
+
+let container=document.getElementById("container")
+
+
+popupBtn.addEventListener ("click",  function (){
     popupOverlay.style.display="block"
     popupBox.style.display="block"
 }
+)
+btnRemove.addEventListener  ("click",function(){
+    popupOverlay.style.display="none"
+    popupBox.style.display="none"
+}
+) 
+btnAdd.addEventListener("click", function(){
+    
+})

34669997d35340adbdbe884f43f51cdab70e1527
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-24 11:17:33 +0530

    add comment

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ let btnRemove=document.getElementById("btn-remove")
 
 let container=document.getElementById("container")
 
-
+// bottom of right end button is this and  function created for that which want to show the popup
 popupBtn.addEventListener ("click",  function (){
     popupOverlay.style.display="block"
     popupBox.style.display="block"
 }
 )
+// create a function for remove the popup message to remove button 
 btnRemove.addEventListener  ("click",function(){
     popupOverlay.style.display="none"
     popupBox.style.display="none"
 }
 ) 
+// add a notes on container box , so we want to  create a new items from this function to add button
 btnAdd.addEventListener("click", function(){
-    
+
 })
